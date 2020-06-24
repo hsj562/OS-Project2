bbfdf392b9fd75e6f60129118acda87a42e4fe7f
root <eayaps@gmail.com>
2020-06-24 19:23:51 +0800

    Mr.Hsueh in my house

--- user_program/my_sample_1_diff.sh
+++ user_program/my_sample_1_diff.sh
@@ -2,14 +2,14 @@
 echo "fcntl diff begin";
 for i in $(seq 1 3)
 do
-	sudo diff ../input/my_input_1/my_input_${i} ../output/my_output_1/fcntl_result_${i}.txt 
+	sudo diff ../input/my_input_1/my_input_${i}.txt ../output/my_output_1/fcntl_result_${i}.txt 
 done
 
 echo "fcntl diff end, mmap diff begin"
 
-for j in $(seq 1 10)
+for j in $(seq 1 3)
 do
-	sudo diff ../input/my_input_1/my_input_${j} ../output/my_output_1/mmap_result_${j}.txt 
+	sudo diff ../input/my_input_1/my_input_${j}.txt ../output/my_output_1/mmap_result_${j}.txt 
 done
 
 echo "mmap diff end"
